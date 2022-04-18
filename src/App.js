327a65a1446ec952770ced4ed80a2b492d1cd41d
wiew7 <dawid074@gmail.com>
2022-04-18 23:30:15 +0200

    readme.md change, created components folder with header component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+import Header from './components/Header'
+
 function App() {
   return (
-    <section className="font-bold text-slate-300 bg-slate-900 h-screen text-7xl grid place-items-center">
-      app
-    </section>
+    <>
+      <Header />
+      <section className="font-bold text-slate-300 bg-slate-900 h-screen text-7xl grid place-items-center">
+        app
+      </section>
+    </>
   );
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+function Header() {
+  return (
+    <header className="fixed top-0 bg-slate-900/60 w-full p-6 text-slate-100">
+        <h1>
+            app.name
+        </h1>
+        <nav>
+
+        </nav>
+    </header>
+  )
+}
+
+export default Header
