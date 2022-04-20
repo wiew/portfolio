b07de50b9f34d87e93b3529b6119b5237a375239
wiew7 <dawid074@gmail.com>
2022-04-20 18:43:21 +0200

    added social icons with links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,11 +4,14 @@ import { NavLink}  from 'react-router-dom'
 import { ReactComponent as MenuIcon } from '../assets/hamburger-menu.svg'
 import { ReactComponent as MenuClose } from '../assets/close.svg'
 
+import { ReactComponent as GithubIcon } from '../assets/github.svg'
+import { ReactComponent as LinkedinIcon } from '../assets/linkedin.svg'
+
 function Header() {
   const [isNavOpen, setIsNavOpen] = useState(false);
 
   return (
-    <header className="fixed top-0 flex justify-between w-full p-6 text-lg bg-black/20 text-slate-100">
+    <header className="fixed top-0 flex items-center justify-between w-full p-6 text-lg bg-gray-900/90 text-slate-100">
         <h1 className="flex-1 font-glitch">
             wiew.dev
         </h1>
@@ -23,8 +26,13 @@ function Header() {
               <MenuClose className='w-7 aspect-square' />
           </button>
         </nav>
-        <div className="flex-1 text-right">
-          socials
+        <div className="flex justify-end flex-1 gap-4 text-right">
+          <a href="https://github.com/wiew">
+            <GithubIcon className='w-6 h-6' />
+          </a>
+          <a href="https://www.linkedin.com/in/dawid-zapała-334a5b171/">
+            <LinkedinIcon className='w-6 h-6' />
+          </a>
         </div>
         <button className={`fixed bg-gray-900/90 p-4 rounded-full bottom-6 transition-all ${ isNavOpen ? "translate-x-full right-0" : "translate-x-0 right-6 duration-500 delay-700" }`} onClick={() => {setIsNavOpen(true)}}>
           <MenuIcon className='w-6 aspect-square'/>
